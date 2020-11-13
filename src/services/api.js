99b19727ac743f8942ec8293d1822a563c9470ce
Ismael Elias <51543534+ismaeElias@users.noreply.github.com>
2020-11-12 22:49:25 -0300

    realizando deploy

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-const api = axios.create({baseURL : 'https://concatenador-pex.herokuapp.com/'});
+const api = axios.create({baseURL : process.env.REACT_APP_API_URL});
 
 export default api;
